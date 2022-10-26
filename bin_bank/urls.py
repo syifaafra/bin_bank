c9d8a5837cd09ac251794022e6accffe02e448b3
Rafi Ghalibin Abrar <rafighalibin@gmail.com>
2022-10-26 20:04:26 +0700

    Add models

---
+++ bin_bank/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from bin_bank.views import show_transaction_user, show_transaction_user_ongoing, show_transaction_user_success, show_transaction_user_failed
+
+
+app_name = 'bin_bank'
+
+urlpatterns = [
+    path('', main, name='main'),  #TODO : homepage 
+    path('json/', show_transaction_user, name='show_transaction_user'), 
+    path('json/ongoing', show_transaction_user_ongoing, name='show_transaction_user_ongoing'),
+    path('json/success', show_transaction_user_success, name='show_transaction_user_success'), 
+    path('json/failed', show_transaction_user_failed, name='show_transaction_user_failed'), 
+    
+]
--- bin_bank/views.py
+++ bin_bank/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from bin_bank.models import Transaction
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
+
+@login_required(login_url='/bin_bank/login/')
+def show_transaction_user(request):
+    tasks = Transaction.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("json", tasks), content_type="application/json")
+
+@login_required(login_url='/bin_bank/login/')
+def show_transaction_user_ongoing(request):
+    tasks = Transaction.objects.filter(user=request.user, status=1)
+    return HttpResponse(serializers.serialize("json", tasks), content_type="application/json")
+
+@login_required(login_url='/bin_bank/login/')
+def show_transaction_user_success(request):
+    tasks = Transaction.objects.filter(user=request.user, status=2)
+    return HttpResponse(serializers.serialize("json", tasks), content_type="application/json")
+
+@login_required(login_url='/bin_bank/login/')
+def show_transaction_user_failed(request):
+    tasks = Transaction.objects.filter(user=request.user, status=3)
+    return HttpResponse(serializers.serialize("json", tasks), content_type="application/json")
