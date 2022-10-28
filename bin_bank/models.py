cc643722b2cb447446e3f408db45a55ac0926a14
Amanda Christie Tarigan <mandachristie04@gmail.com>
2022-10-28 10:05:59 +0700

    add styles and navbar

--- bin_bank/models.py
+++ bin_bank/models.py
@@ -20,5 +20,16 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     points = models.IntegerField()
 
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    thumbnail = models.CharField(max_length=50)
+    slug = models.SlugField()
+    intro = models.TextField()
+    image= models.CharField(max_length=500)
+    news = models.TextField()
+    source = models.TextField()
 
-
+class Feedback(models.Model):
+    date = models.DateTimeField(default=datetime.now())
+    feedback = models.TextField()
+    
